0e0325adbec038aad73700a5a84a64f998b6abaf
Mario_Brnal10 <145825327+MarioBrnal10@users.noreply.github.com>
2025-04-06 16:28:41 -0600

    cambios

--- DB/conexion.py
+++ DB/conexion.py
@@ -1,21 +1,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from DB.base import Base  # Usamos Base desde archivo separado
-import os
 
-# Nombre del archivo de base de datos SQLite
-DB_NAME = "tienda_comics.sqlite"
+# Datos de conexión
+USER = "root"
+PASSWORD = ""  # Si tienes contraseña, colócala aquí
+HOST = "localhost"
+PORT = "3306"
+DB_NAME = "tienda_comics"
 
-# Ruta del archivo (opcionalmente puedes usar os.path para rutas absolutas)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, DB_NAME)}"
+# URL de conexión a MySQL con PyMySQL
+DATABASE_URL = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
 
 # Crear el motor de conexión
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},  # Necesario para SQLite con FastAPI
-    echo=True
-)
+engine = create_engine(DATABASE_URL, echo=True)
 
 # Crear la sesión local
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
