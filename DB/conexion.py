a28e797167dd3ca3f8c4ff5fa715fb79d4061b45
Mario_Brnal10 <145825327+MarioBrnal10@users.noreply.github.com>
2025-04-08 14:23:58 -0600

    arreglo

--- DB/conexion.py
+++ DB/conexion.py
@@ -1,23 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from DB.base import Base  # Usamos Base desde archivo separado
 
 # Datos de conexión
 USER = "root"
-PASSWORD = "YUXnonFHtyVmERdMECmNmlFsYVQXWtAL"  # Si tienes contraseña, colócala aquí
+PASSWORD = "YUXnonFHtyVmERdMECmNmlFsYVQXWtAL"
 HOST = "shortline.proxy.rlwy.net"
 PORT = "20787"
 DB_NAME = "railway"
 
-# URL de conexión a MySQL con PyMySQL
+# URL de conexión con PyMySQL
 DATABASE_URL = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
 
-# Crear el motor de conexión
+# Crear el motor de SQLAlchemy
 engine = create_engine(DATABASE_URL, echo=True)
 
-# Crear la sesión local
+# Crear sesión local
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Generador de sesión (para usar en dependencias de FastAPI)
 def get_db():
     db = Session()
     try:
